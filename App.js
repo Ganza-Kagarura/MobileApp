0fedd742b87d5f8138eab32bd43acbb543fe4974
Ganza-Kagarura <isaacganza22@gmail.com>
2024-02-13 21:28:32 +0200

    bottom & left tab

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import Calculator from "./Screens/Calculator"
+import Home from "./Screens/Home"
+import Contacts from "./Screens/Contacts"
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Calculator') {
+              iconName = 'calculator';
+            } else if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Contacts') {
+              iconName = 'contacts';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Calculator" component={Calculator} />
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Contacts" component={Contacts} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
---
+++ Screens/Contacts.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View } from "react-native"
+import { Text } from "react-native"
+
+export default function Contacts () {
+    return (
+        <View>
+            <Text>Name: Ganza</Text>
+            <Text>ID: 24290</Text>
+        </View>
+    )
+}
